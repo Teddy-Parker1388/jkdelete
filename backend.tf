b782ce0525d17dbb03dcb3ac7e9ea4349ebd9f2a
Teddy-Parker1388 <pteddy17@gmail.com>
2023-03-19 17:05:05 +0000

    Modified four files. Added one file

--- backend.tf
+++ backend.tf
@@ -4,8 +4,8 @@ terraform {
  backend "s3" {
    bucket = "team17champions"
    key = "team17state/team17.tfstate"
-   region = "us-east-1"
-   dynamodb_table = "team17table"
+   region = "us-east-2"
+   dynamodb_table = "team18table"
    encrypt = true
    
    
